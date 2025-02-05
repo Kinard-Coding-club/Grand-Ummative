70543829858c900bddc4190f24a4ff62c3cf164d
90372 <90372@psdschools.org>
2025-02-05 22:49:14 +0000

    Added a skeleton for our code

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-import time
+#headers
+import subprocess
+import sys
+import pygame
 
-print("Hello, world")
+#start
+def main():
+    print("----- DO NOT CLOSE THIS WINDOW! -----")
+    pygame.init()
+
+    #Start the main loop
+    running = True
+    while running:
+        #Heartbeat worker and indicator
+        print("Heartbeat")
+
+#make sure the entry point is execution before executing main code
+if __name__ == "__main__":
+    main()
+#end
