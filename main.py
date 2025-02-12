89d43c600e438f08c6fbf5fbd0d789ad35bc594c
Adrian Ray LeDeaux <138169736+TheW0NK@users.noreply.github.com>
2025-02-12 22:51:09 +0000

    ...

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+#Headers
+import pygame
+import traceback
+import sys
+import os
+import random
+import time
+
+
+#Initalize all variables
+counter = 0
+line = 0
+last = null
+process = null
+#Start
 print("----- DO NOT CLOSE THIS WINDOW! -----")
+def main():
+    pygame.init()
+    if process != null:
+        print(f"{line}:Heartbeat {counter}: Got a registry event at {last} using {process}")
+if __name__ == "__main__":
+    main()
+#end
