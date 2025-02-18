04c19173d09037fdce69740ea861cb379e3daf07
Adrian Ray LeDeaux <138169736+TheW0NK@users.noreply.github.com>
2025-02-18 17:29:54 +0000

    Added Pygame Window and keydown event handler

--- main.py
+++ main.py
@@ -7,15 +7,43 @@ import random
 import time
 
 # Initialize all variables
+
+global counter, line, last, process, key, location
 counter = 0
 line = 0
 last = 'null'
 process = 'null'
+key = 'null'
+location = 'loader'
+
 #Start
 print("----- DO NOT CLOSE THIS WINDOW! -----")
 def main():
     pygame.init()
-    
+    # Define all Pygame events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                last = 'K_RETURN'
+                process = 'KEYDOWN-HANDLER'
+                key = 'RETURN'
+            elif event.key == pygame.K_ESCAPE:
+                key = 'ESCAPE'
+                last = 'K_ESCAPE'
+                process = 'KEYDOWN-HANDLER'
+                location = 'pause'
+            else:
+                counter += 1
+                line += 1
+                last = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                process = 'KEYDOWN'
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("🗿 I ate the name for lunch. Good game, I promise!")
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
     if process != 'null':
         print(f"{line}:Heartbeat {counter}: Got a registry event at {last} using {process}")
 if __name__ == "__main__":
