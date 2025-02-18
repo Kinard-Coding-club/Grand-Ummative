15d5e95cf0e79634dfcfe9a3627c4b0f7c7a64a3
Adrian Ray LeDeaux <138169736+TheW0NK@users.noreply.github.com>
2025-02-18 16:50:57 +0000

    Added a .saves file and a readme.md file

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import time
 #Initalize all variables
 counter = 0
 line = 0
-last = null
-process = null
+last = 'null'
+process = 'null'
 #Start
 print("----- DO NOT CLOSE THIS WINDOW! -----")
 def main():
     pygame.init()
-    # Place your code here
+    
     if process != null:
         print(f"{line}:Heartbeat {counter}: Got a registry event at {last} using {process}")
 if __name__ == "__main__":
