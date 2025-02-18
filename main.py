d19e8220117a59804bea9960d79ec5ea91684b77
Adrian Ray LeDeaux <138169736+TheW0NK@users.noreply.github.com>
2025-02-18 17:21:02 +0000

    Removed docker environment checking tool

--- main.py
+++ main.py
@@ -13,21 +13,10 @@ last = 'null'
 process = 'null'
 #Start
 print("----- DO NOT CLOSE THIS WINDOW! -----")
-if os.name == 'nt':
-    print("Using NTOS (Not a dockerable!)")
-else:
-    print("Using Linux. Checking /dev directory")
-    if os.name != 'nt':
-        if os.path.exists('/dev'):
-            print("/dev directory exists.")
-        else:
-            print("/dev directory does not exist. Entering web mode...")
-            docker = 'true'
-
 def main():
     pygame.init()
     
-    if process != null:
+    if process != 'null':
         print(f"{line}:Heartbeat {counter}: Got a registry event at {last} using {process}")
 if __name__ == "__main__":
     main()
